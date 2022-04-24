1dd8f6c6e73f6b5a6533242f48f4c267b068e0bc
dushaokun <17802917036@163.com>
2022-04-24 22:27:15 +0800

    练习2

---
+++ hello.py
@@ -0,0 +1,30 @@
+# print("Hello, World!")
+# L = ['ABC','BDF','GHU','KKK']
+# print([s.lower() for s in L])
+
+# 基础知识
+"""基础知识转义字符"""
+print('I\'m ok!')
+print('I\'m learning\npython')
+# print('\\\n\\')
+# 注释快捷键 crtl+/
+"""
+这是多行注释
+这是多行注释
+"""
+# 变量名必须由数字、字母、下划线组成，且数字不能开头
+# 变量名一般用小写字母加下划线组成
+# 不能使用关键字和已有的名称
+# 变量是用来存储程序运行中所需要的数据的
+# 变量名一定有意义
+
+my_str = "ryiweufsdjg"
+my_type = type(my_str)
+print(my_type)
+my_float = 3.1415926
+my_type = type(my_float)
+print(my_type)
+
+
+
+
