9ba8a522645dfbe6ec0c9133a9967273f38167b7
dushaokun <17802917036@163.com>
2022-06-08 23:47:45 +0800

    if 语句练习

---
+++ 两个数相除-课堂练习.py
@@ -0,0 +1,9 @@
+# num1通过input输入的数字
+# num2通过input输入的数字
+# 如果num2不等于0，计算num1除以num2的结果
+num1 = float(input("请输入num1:"))
+num2 = float(input("请输入num2:"))
+if num2 != 0:
+    print(
+        num1/num2
+    )
---
+++ 第一个if语句.py
@@ -0,0 +1,4 @@
+a = 10
+if a < 5:
+    print("小于5的值")  # 这里的print和if是一个代码块
+print("hello")  # 这里的print和if是没有关系
